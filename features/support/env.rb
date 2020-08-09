918484324b06a883c8fed9da544276654e7609e5
Carla dos Santos Victal <carla.victal@mrv.com.br>
2020-08-09 15:38:38 -0300

    add project

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,6 @@ require "selenium-webdriver"
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
-  #config.app_host = "https://www.americanas.com.br/"
+  #config.app_host = "https://www.magazineluiza.com.br/"
   config.default_max_wait_time = 10
 end
